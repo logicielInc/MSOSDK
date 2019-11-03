c9a02fab2d72df0a2e3dc27592851458a2ebcfa2
jsetting <jsetting32@yahoo.com>
2019-11-03 12:15:02 -0800

    Initial Commit

---
+++ MSOSDK/MSOSDK.h
@@ -0,0 +1,13 @@
+//
+//  MSOSDK.h
+//  MSOSDK
+//
+//  Created by Setting, John on 11/3/19.
+//  Copyright © 2019 Logiciel Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSOSDK : NSObject
+
+@end
---
+++ MSOSDK/MSOSDK.m
@@ -0,0 +1,13 @@
+//
+//  MSOSDK.m
+//  MSOSDK
+//
+//  Created by Setting, John on 11/3/19.
+//  Copyright © 2019 Logiciel Inc. All rights reserved.
+//
+
+#import "MSOSDK.h"
+
+@implementation MSOSDK
+
+@end
